e4d4a68700593ab4aae789c62fb98de467399067
Jorren schepers <s094476@ap.be>
2018-05-26 16:12:28 +0200

    small property name changes

--- API/Controllers/Model/Car.cs
+++ API/Controllers/Model/Car.cs
@@ -1,15 +1,16 @@
 namespace Model{
     public class Car{
-        public int BuildYear { get; set; }
         public string Model { get; set; }
         public int HorsePower { get; set; }
         public string Engine { get; set; }
-        public int FuelConsumption { get; set; }        // Litres/100km
+        public double FuelConsumption { get; set; }        // Litres/100km
         public PowerSource PowerSource { get; set; }
         public int Seats { get; set; }                  //max amount of passengers
+        public int StartingPrice { get; set; }
     }
 
     public enum PowerSource{
+        Benzine,
         Diesel,
         Electric,
         Hybrid,
--- API/Controllers/Model/Manufacturer.cs
+++ API/Controllers/Model/Manufacturer.cs
@@ -3,6 +3,9 @@ namespace Model{
         public string Name { get; set; }
         public string Origin { get; set; }          //country of origin
         public int FoundedIn { get; set; }          //year of founding
-        public int LastYearRevenue { get; set; }    //revenue from last year
+        public string Founder { get; set; }
+        public double LastYearRevenue { get; set; }    //revenue from last year in millions (€)
+        public double LastYearProfit { get; set; }     //profit from last year in millions (€)
+        public string HomePage { get; set; }
     }
 }
