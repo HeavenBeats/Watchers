bf0782904248a545c504bfe9ffb24fcca065f64d
Jorren schepers <s094476@ap.be>
2018-05-26 17:21:55 +0200

    added carscontroller with basic routers for GET, POST, PUT and DELETE

---
+++ API/Controllers/CarsController.cs
@@ -0,0 +1,75 @@
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Model;
+
+namespace API.Controllers{
+    [Route("/api/cars")]
+    public class CarsController : Controller
+    {
+        private readonly CarDbContext context;
+
+        public CarsController(CarDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public IActionResult getCars(){
+            IQueryable<Car> query = context.Cars;
+
+            return Ok(query.Include(c => c.Manufacturer).Select(c => c).ToList());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult getCar(int id){
+            var car = context.Cars.Include(c => c.Manufacturer)
+                                    .Where(c => c.Id == id)
+                                    .Select(c => c);
+            if(car == null)
+                return NotFound();
+            
+            return Ok(car);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult deleteCar(int id){
+            var car = context.Cars.Find(id);
+            if(car == null)
+                return NotFound();
+
+            context.Cars.Remove(car);
+            context.SaveChanges();    
+            return NoContent();
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult updateCar(int id, [FromBody] Car updateCar){
+            if(updateCar == null || updateCar.Id != id)
+                return BadRequest();
+            
+            var oldCar = context.Cars.Find(updateCar.Id);
+            if(oldCar == null)
+                return NotFound();
+            
+            
+            oldCar.Model = updateCar.Model;
+            oldCar.HorsePower = updateCar.HorsePower;
+            oldCar.Engine = updateCar.Engine;
+            oldCar.FuelConsumption = updateCar.FuelConsumption;
+            oldCar.PowerSource = updateCar.PowerSource;
+            oldCar.Seats = updateCar.Seats;
+            oldCar.StartingPrice = updateCar.StartingPrice;
+            oldCar.Manufacturer = updateCar.Manufacturer;
+            context.SaveChanges();
+            return Ok(oldCar);
+        }
+
+        [HttpPost]
+        public IActionResult createCar([FromBody] Car newCar){
+            context.Cars.Add(newCar);
+            context.SaveChanges();
+            return Created("", newCar);
+        }
+    }
+}
