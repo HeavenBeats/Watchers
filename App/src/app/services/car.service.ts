a5f22251284bd3d48052ebe56c384df06e8aac46
Jorren schepers <s094476@ap.be>
2018-05-27 14:17:41 +0200

    added basic info on carscomponent

--- App/src/app/services/car.service.ts
+++ App/src/app/services/car.service.ts
@@ -5,9 +5,19 @@ import { Observable } from "rxjs/Observable";
 
 @Injectable()
 export class CarService{
+    car : Car;
+
     constructor(private http : HttpClient){}
 
     public getCars() : Observable<Car[]>{
         return this.http.get<Car[]>('http://localhost:5000/api/cars');
+    }
+
+    public getCar(id : number) : Observable<Car>{
+        return this.http.get<Car>('http://localhost:5000/cars/' + id);
+    }
+
+    public setCar(id : number){
+        this.getCar(id).subscribe(c => this.car = c);
     }
 }
