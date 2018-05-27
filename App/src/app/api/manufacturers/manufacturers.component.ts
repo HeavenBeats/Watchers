234b931fcb2b0ca27bc132e0e8009ea097e98dbe
Jorren schepers <s094476@ap.be>
2018-05-27 18:52:15 +0200

    changed carservice to also implement manufacturer for editing

--- App/src/app/api/manufacturers/manufacturers.component.ts
+++ App/src/app/api/manufacturers/manufacturers.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Manufacturer } from '../../model/manufacturer';
+import { CarService } from '../../services/car.service';
 
 @Component({
     selector: "api-manufacturers",
@@ -9,12 +10,13 @@ import { Manufacturer } from '../../model/manufacturer';
 export class ManufacturersComponent implements OnInit{
     manufacturers : Manufacturer[]
 
+    constructor(private CarSvc : CarService){}
 
     ngOnInit(){
-        
+        this.CarSvc.getManufacturers().subscribe(m => this.manufacturers = m);
     }
 
     public setManufacturer(id : number){
-
+        this.CarSvc.setManufacturer(id);
     }
 }
--- App/src/app/services/car.service.ts
+++ App/src/app/services/car.service.ts
@@ -2,10 +2,12 @@ import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { Car } from "../model/car";
 import { Observable } from "rxjs/Observable";
+import { Manufacturer } from "../model/manufacturer";
 
 @Injectable()
 export class CarService{
     public car : Car;
+    public manufacturer : Manufacturer;
 
     constructor(private http : HttpClient){}
 
@@ -19,10 +21,24 @@ export class CarService{
 
     public setCar(id : number){
         this.getCar(id).subscribe(c => this.car = c);
+        this.manufacturer = null;
     }
 
     public updateCar(car : Car) : Observable<Car>{
         console.log(car);
         return this.http.put<Car>('http://localhost:5000/api/cars/' + car.id, car);
+    }
+
+    public getManufacturers() : Observable<Manufacturer[]>{
+        return this.http.get<Manufacturer[]>('http://localhost:5000/api/manufacturers');
+    }
+
+    public getManufacturer(id : number) : Observable<Manufacturer>{
+        return this.http.get<Manufacturer>('http://localhost:5000/api/manufacturers/' + id);
+    }
+
+    public setManufacturer(id : number){
+        this.getManufacturer(id).subscribe(m => this.manufacturer = m);
+        this.car = null;
     }
 }
