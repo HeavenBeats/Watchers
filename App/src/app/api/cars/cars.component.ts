95714f8ef975b417707be333b06467e7ee0cfaa7
Jorren schepers <s094476@ap.be>
2018-05-27 14:09:13 +0200

    added cars component basics

---
+++ App/src/app/api/cars/cars.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { Car } from '../../model/car';
+import { CarService } from '../../services/car.service';
+
+@Component({
+    selector: "api-cars",
+    templateUrl: "cars.component.html",
+    //styleUrls: ["cars.component.scss"]
+})
+export class CarsComponent implements OnInit{
+    cars : Car[]
+
+    constructor(private CarSvc : CarService){}
+
+    ngOnInit(){
+        this.CarSvc.getCars().subscribe(c => this.cars = c);
+    }
+}
