9f73bd752ea364737933e783346bf08702a787d7
Jorren schepers <s094476@ap.be>
2018-05-28 20:04:26 +0200

    small layout changes and 'creating new' form
    - fixed padding on header-btn class, 10px
    - added possibility to fill in new car/manufacturer
    -> does not really save yet

--- App/src/app/api/editdetail/editdetail.component.ts
+++ App/src/app/api/editdetail/editdetail.component.ts
@@ -8,77 +8,132 @@ import { Manufacturer } from "../../model/manufacturer";
     templateUrl: "editdetail.component.html",
     styleUrls: ["editdetail.component.scss"]
 })
-export class EditDetailComponent implements OnInit{
-    editingCar : boolean;
-    editCar : Car; 
-    editingManufacturer : boolean;
-    editManufacturer : Manufacturer;
-    manufacturers : Manufacturer[];
-    manId : number;
+export class EditDetailComponent implements OnInit {
+    editingCar: boolean;
+    editCar: Car;
+    editingManufacturer: boolean;
+    editManufacturer: Manufacturer;
+    manufacturers: Manufacturer[];
+    manId: number;
+    newCar: boolean;
+    newManufacturer: boolean;
 
-    get EditCar() : Car{
+    get EditCar(): Car {
         return this.editCar;
     }
 
-    set EditCar(car : Car){
+    set EditCar(car: Car) {
         this.editCar = car;
     }
 
-    get EditManufacturer() : Manufacturer{
+    get EditManufacturer(): Manufacturer {
         return this.editManufacturer;
     }
 
-    set EditManufacturer(manufacturer : Manufacturer){
+    set EditManufacturer(manufacturer: Manufacturer) {
         this.editManufacturer = manufacturer;
     }
 
-    get ManId() : number{
+    get ManId(): number {
         return this.manId;
     }
 
-    set ManId(id : number){
+    set ManId(id: number) {
         this.manId = id;
     }
 
-    constructor(private CarSvc : CarService){}
+    constructor(private CarSvc: CarService) { }
 
-    ngOnInit(){
+    ngOnInit() {
         this.CarSvc.getManufacturers().subscribe(m => this.manufacturers = m);
+        setInterval(() => {
+            if (this.CarSvc.car != null || this.CarSvc.manufacturer != null) {
+                this.newCar = false;
+                this.newManufacturer = false;
+            }
+        }, 100);
     }
 
-    public EditingCar(){
-        if(this.editingCar){
+    public EditingCar() {
+        if (this.editingCar) {
             this.editingCar = false;
             this.editCar = null;
         }
-        else{
+        else {
             this.editingCar = true;
             this.editCar = this.CarSvc.car;
             this.manId = this.editCar.manufacturer.id;
         }
     }
 
-    public EditingManufacturer(){
-        if(this.editingManufacturer){
+    public EditingManufacturer() {
+        if (this.editingManufacturer) {
             this.editingManufacturer = false;
             this.editManufacturer = null;
         }
-        else{
+        else {
             this.editingManufacturer = true;
             this.editManufacturer = this.CarSvc.manufacturer;
         }
     }
 
-    public SaveCar(){
+    public SaveCar() {
         this.editCar.manufacturer = this.manufacturers[this.ManId];
         this.CarSvc.updateCar(this.editCar);
         this.editingCar = false;
         this.editCar = null;
     }
 
-    public SaveManufacturer(){
+    public SaveManufacturer() {
         this.CarSvc.updateManufacturer(this.editManufacturer).subscribe(m => console.log(m));
         this.editingManufacturer = false;
         this.editManufacturer = null
     }
+
+    public NewCar() {
+        this.newCar = true;
+        this.CarSvc.car = null;
+        this.CarSvc.manufacturer = null;
+        this.editCar = <Car>{};
+    }
+
+    public NewManufacturer() {
+        this.newManufacturer = true;
+        this.CarSvc.manufacturer = null;
+        this.CarSvc.car = null;
+        this.editManufacturer = <Manufacturer>{};
+    }
+
+    public CreateCar() {
+        this.editCar.manufacturer = this.manufacturers[this.manId];
+        if (this.ReadyToSaveCar()) {
+            this.newCar = false;
+            this.CarSvc.car = this.editCar;
+            this.editCar = null;
+        }
+        else {
+            alert("please fill in all the fields");
+        }
+    }
+
+    public CreateManufacturer() {
+        if (this.ReadyToSaveManufacturer()) {
+            this.newManufacturer = false;
+            this.CarSvc.manufacturer = this.editManufacturer;
+            this.editManufacturer = null;
+        }
+        else{
+            alert("please fill in all the fields");
+        }
+    }
+
+    public ReadyToSaveCar() {
+        return this.editCar.model != null && this.EditCar.engine != null && this.editCar.horsePower != null && this.editCar.fuelConsumption != null && this.editCar.powerSource != null && this.editCar.seats != null && this.editCar.startingPrice != null && this.editCar.manufacturer != null;
+    }
+
+    public ReadyToSaveManufacturer() {
+        return this.editManufacturer.name && this.editManufacturer.origin != null && this.editManufacturer.foundedIn != null && this.editManufacturer.founder != null && this.editManufacturer.lastYearProfit != null && this.editManufacturer.lastYearRevenue != null && this.editManufacturer.homePage != null;
+    }
+
+
 }
