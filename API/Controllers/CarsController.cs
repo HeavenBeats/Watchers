a280f30bfc5f6cc5674e2e5487be6c55eb2e6037
Jorren schepers <s094476@ap.be>
2018-05-29 14:02:42 +0200

    setting manufacturer before adding/updating car moved to method

--- API/Controllers/CarsController.cs
+++ API/Controllers/CarsController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Cors;
@@ -5,7 +6,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Model;
 
-namespace API.Controllers{
+namespace API.Controllers
+{
     [EnableCors("AllowAllHeaders")]
     [Route("/api/cars")]
     public class CarsController : Controller
@@ -18,45 +20,50 @@ namespace API.Controllers{
         }
 
         [HttpGet]
-        public IActionResult getCars(){
+        public IActionResult getCars()
+        {
             IQueryable<Car> query = context.Cars;
 
             return Ok(query.Include(c => c.Manufacturer).Select(c => c).ToList());
         }
 
         [HttpGet("{id}")]
-        public IActionResult getCar(int id){
+        public IActionResult getCar(int id)
+        {
             var car = context.Cars.Include(c => c.Manufacturer)
                                     .Where(c => c.Id == id)
                                     .Select(c => c)
                                     .Single();
-            if(car == null)
+            if (car == null)
                 return NotFound();
-            
+
             return Ok(car);
         }
 
         [HttpDelete("{id}")]
-        public IActionResult deleteCar(int id){
+        public IActionResult deleteCar(int id)
+        {
             var car = context.Cars.Find(id);
-            if(car == null)
+            if (car == null)
                 return NotFound();
 
             context.Cars.Remove(car);
-            context.SaveChanges();    
+            context.SaveChanges();
             return NoContent();
         }
 
         [HttpPut("{id}")]
-        public IActionResult updateCar(int id, [FromBody] Car updateCar){
-            if(updateCar == null || updateCar.Id != id)
+        public IActionResult updateCar(int id, [FromBody] Car updateCar)
+        {
+            if (updateCar == null || updateCar.Id != id)
                 return BadRequest();
-            
+
             var oldCar = context.Cars.Find(updateCar.Id);
-            if(oldCar == null)
+            if (oldCar == null)
                 return NotFound();
-            
-            
+
+
+            Console.Write("test");
             oldCar.Model = updateCar.Model;
             oldCar.HorsePower = updateCar.HorsePower;
             oldCar.Engine = updateCar.Engine;
@@ -64,24 +71,36 @@ namespace API.Controllers{
             oldCar.PowerSource = updateCar.PowerSource;
             oldCar.Seats = updateCar.Seats;
             oldCar.StartingPrice = updateCar.StartingPrice;
-            
-            List<Manufacturer> manufacturers = context.Manufacturers.ToList();
-            Manufacturer updateManufacturer = null;
-            manufacturers.ForEach(m =>{
-                if(m.Id == updateCar.Manufacturer.Id)
-                    updateManufacturer = m;
-            });
-            if(updateManufacturer != null)
+
+            Manufacturer updateManufacturer = setManufacturer(updateCar);
+            if (updateManufacturer != null)
                 oldCar.Manufacturer = updateManufacturer;
             context.SaveChanges();
             return Ok(oldCar);
         }
 
         [HttpPost]
-        public IActionResult createCar([FromBody] Car newCar){
+        public IActionResult createCar([FromBody] Car newCar)
+        {
+            Manufacturer updateManufacturer = setManufacturer(newCar);
+            Console.Write("test");
+            if (updateManufacturer != null)
+                newCar.Manufacturer = updateManufacturer;
             context.Cars.Add(newCar);
             context.SaveChanges();
             return Created("", newCar);
+        }
+
+        private Manufacturer setManufacturer(Car car)
+        {
+            List<Manufacturer> manufacturers = context.Manufacturers.ToList();
+            Manufacturer updateManufacturer = null;
+            manufacturers.ForEach(m =>
+            {
+                if (m.Id == car.Manufacturer.Id)
+                    updateManufacturer = m;
+            });
+            return updateManufacturer;
         }
     }
 }
