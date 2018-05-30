64a6bf32ec53475eae7f482190c4a0a1f721a477
Jorren schepers <s094476@ap.be>
2018-05-30 19:49:42 +0200

    added background to searchcomponent

--- App/src/app/search/search.component.ts
+++ App/src/app/search/search.component.ts
@@ -1,18 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { SearchService } from '../services/search.service';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
-  selector: 'search',
-  templateUrl: './search.component.html',
-  //styleUrls: ['./search.component.scss']
+    selector: 'search',
+    templateUrl: './search.component.html',
+    //styleUrls: ['./search.component.scss']
 })
 export class SearchComponent implements OnInit {
 
-    constructor(private route : ActivatedRoute, private SearchSvc : SearchService) {
+    constructor(private route: ActivatedRoute, private SearchSvc: SearchService, private sanitizer: DomSanitizer) {
     }
 
-    ngOnInit(){
+    ngOnInit() {
         this.route.params.subscribe(p => this.SearchSvc.Search = p['search'])
+    }
+
+    public getBackground() {
+        return this.sanitizer.bypassSecurityTrustStyle(`url("https://qz.com/wp-content/uploads/2018/01/movie-theater-2-1.jpg?quality=80&strip=all&w=2400")`)
     }
 }
