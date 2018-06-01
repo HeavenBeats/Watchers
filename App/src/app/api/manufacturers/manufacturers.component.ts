fea025223eb7fb3aa20c20f7c061080e41be82be
Jorren schepers <s094476@ap.be>
2018-06-01 10:03:59 +0200

    implemented sorting and paging for manufacturerscomponenet

--- App/src/app/api/manufacturers/manufacturers.component.ts
+++ App/src/app/api/manufacturers/manufacturers.component.ts
@@ -8,14 +8,53 @@ import { CarService } from '../../services/car.service';
     //styleUrls: ["manufacturers.component.scss"]
 })
 export class ManufacturersComponent implements OnInit{
+    sortOptions: string[] = [
+        "Name",
+        "Origin",
+        "Founded in",
+        "Founder",
+        "Revenue",
+        "Profit"
+    ];
+    sortOption: string = this.sortOptions[0];
+    pages : number[];
+    page : number = 1;
+
+    set SortOption(s: string) {
+        this.sortOption = s;
+        this.ReloadManufacturers();
+    }
+
+    get SortOption(): string {
+        return this.sortOption;
+    }
+
+    set Page(p : number){
+        this.page = p;
+        this.ReloadManufacturers();
+    }
+
+    get Page() : number{
+        return this.page;
+    }
 
     constructor(private CarSvc : CarService){}
 
     ngOnInit(){
-        this.CarSvc.getManufacturers().subscribe(m => this.CarSvc.manufacturers = m);
+        this.ReloadManufacturers();
     }
 
     public setManufacturer(id : number){
         this.CarSvc.setManufacturer(id);
+    }
+
+    ReloadManufacturers(){
+        this.CarSvc.getManufacturers(this.sortOption, this.page).subscribe(m => {
+            this.CarSvc.manufacturers = m.manufacturerResults;
+            this.pages = [];
+            for(var i = 1; i <= m.pages; i++){
+                this.pages.push(i);
+            }
+        });
     }
 }
