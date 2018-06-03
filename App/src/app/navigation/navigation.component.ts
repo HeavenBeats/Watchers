532c48c0a4d3056351bd0dbc360bfbb7655ca521
Jorren schepers <s094476@ap.be>
2018-06-03 13:32:33 +0200

    added logout button and hide elements when not logged in

--- App/src/app/navigation/navigation.component.ts
+++ App/src/app/navigation/navigation.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService, SocialUser } from 'angular4-social-login';
 
 @Component({
   selector: 'navigation',
@@ -7,10 +8,23 @@ import { Router } from '@angular/router';
   //styleUrls: ['./navigation.component.scss']
 })
 export class NavComponent {
+  private user: SocialUser;
+  private loggedIn: boolean;
 
-  constructor(private router : Router){}
+  constructor(private router : Router, private authService: AuthService){}
 
   public submit(search : string){
     this.router.navigate(['/search', search]);
+  }  
+
+  ngOnInit() {
+    this.authService.authState.subscribe((user) => {
+      this.user = user;
+      this.loggedIn = (user != null);
+    });
+  }
+
+  Logout(){
+    this.authService.signOut();
   }
 }
