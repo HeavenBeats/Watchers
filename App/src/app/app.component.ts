a18780822a1ba44d48e9f7aca96c5ad9c2a5b67d
Jorren schepers <s094476@ap.be>
2018-06-03 13:30:27 +0200

    added authentication to showing content

--- App/src/app/app.component.ts
+++ App/src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService, SocialUser } from "angular4-social-login";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  private user: SocialUser;
+  private loggedIn: boolean;
+  
+  constructor(private authService: AuthService) { }
+
+  ngOnInit() {
+    this.authService.authState.subscribe((user) => {
+      this.user = user;
+      this.loggedIn = (user != null);
+    });
+  }
 }
