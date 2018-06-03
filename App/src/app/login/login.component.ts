a6d602bd7745f94bd1c414d11518f1d6db9cc36e
Jorren schepers <s094476@ap.be>
2018-06-03 13:31:14 +0200

    layout changes and authentication
    - added background
    - edited card layout and buttons
    - added functionality to buttons for google and facebook login

--- App/src/app/login/login.component.ts
+++ App/src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthService, GoogleLoginProvider, FacebookLoginProvider } from "angular4-social-login";
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'login',
@@ -6,4 +8,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  
+  constructor(private authService: AuthService, private sanitizer: DomSanitizer) { }
+
+  signInWithGoogle(): void {
+    this.authService.signIn(GoogleLoginProvider.PROVIDER_ID);
+  }
+
+  signInWithFB(): void {
+    this.authService.signIn(FacebookLoginProvider.PROVIDER_ID);
+  }
+
+  public getBackground() {
+    return this.sanitizer.bypassSecurityTrustStyle(`url("https://www.nyfa.edu/film-school-blog/wp-content/uploads/2015/01/5779401142_78dd986417_o1.jpg")`)
+}
 }
